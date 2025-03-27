51ae65d1ba92fe56317ff6aeda1316847981362f
goldbeedev <goldbee500@gmail.com>
2025-03-27 17:15:33 -0500

    fix other type thing

--- src/app/lib/stripe.ts
+++ src/app/lib/stripe.ts
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2023-10-16",
   appInfo: {
     name: "SOME_ENV",
     url: "SOME_ENV",
